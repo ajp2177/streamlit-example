0eecaee5c1c9f91fdd1dfb7a373ab25dcd46f497
ajp2177 <118642994+ajp2177@users.noreply.github.com>
2022-11-27 15:19:26 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,36 +4,38 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-"""
 
 with st.echo(code_location='below'):
-    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png")
     pages = ["Home", "Player Exploration", "Predictive Model"]
-    st.sidebar.selectbox("Select a page:", pages)
-    st.markdown("Select type of player:")
-    st.radio(" ", ["Batter", "Pitcher"], horizontal=True)
-    players = ["Mike Trout", "Aaron Judge", "Mookie Betts", "Jose Altuve"]
-    st.selectbox("Player:", players)
-    
-    
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    choice = st.sidebar.selectbox("Select a page:", pages)
+    if choice == "Home":
+        """
+        # Welcome to Streamlit!
+        """
+        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png")
+    elif choice == "Player Exploration":
+        st.markdown("Select type of player:")
+        st.radio(" ", ["Batter", "Pitcher"], horizontal=True)
+        players = ["Mike Trout", "Aaron Judge", "Mookie Betts", "Jose Altuve"]
+        st.selectbox("Player:", players)
+    elif choice == "Predictive Model":
+        
+        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        Point = namedtuple('Point', 'x y')
+        data = []
 
-    points_per_turn = total_points / num_turns
+        points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        for curr_point_num in range(total_points):
+            curr_turn, i = divmod(curr_point_num, points_per_turn)
+            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+            radius = curr_point_num / total_points
+            x = radius * math.cos(angle)
+            y = radius * math.sin(angle)
+            data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
