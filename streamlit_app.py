443fb5153425495bb97fef7f1b7fde4c5fbffebd
ajp2177 <118642994+ajp2177@users.noreply.github.com>
2022-11-27 15:52:47 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,14 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-
+"""
+        # Welcome to Streamlit!
+        """
 
 with st.echo(code_location='below'):
     pages = ["Home", "Player Exploration", "Predictive Model"]
     choice = st.sidebar.selectbox("Select a page:", pages)
     if choice == "Home":
-        """
-        # Welcome to Streamlit!
-        """
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png")
     elif choice == "Player Exploration":
         st.markdown("Select type of player:")
