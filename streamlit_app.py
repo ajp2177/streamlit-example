553f0ae52a8bc8e591d12ce3dd8d3f2c30ff9978
ajp2177 <118642994+ajp2177@users.noreply.github.com>
2022-11-27 15:56:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ import streamlit as st
         # Welcome to Streamlit!
         """
 
-with st.echo(code_location='below'):
-    pages = ["Home", "Player Exploration", "Predictive Model"]
-    choice = st.sidebar.selectbox("Select a page:", pages)
-    if choice == "Home":
+#with st.echo(code_location='below'):
+pages = ["Home", "Player Exploration", "Predictive Model"]
+choice = st.sidebar.selectbox("Select a page:", pages)
+if choice == "Home":
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png")
-    elif choice == "Player Exploration":
+elif choice == "Player Exploration":
         st.markdown("Select type of player:")
         st.radio(" ", ["Batter", "Pitcher"], horizontal=True)
         players = ["Mike Trout", "Aaron Judge", "Mookie Betts", "Jose Altuve"]
         st.selectbox("Player:", players)
-    elif choice == "Predictive Model":
+elif choice == "Predictive Model":
         print("hello")
