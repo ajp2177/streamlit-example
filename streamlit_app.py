18d391cf0f403af8e7cee87efb40f797694b265b
ajp2177 <118642994+ajp2177@users.noreply.github.com>
2022-11-27 16:31:52 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,14 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-"""
-        # Welcome to Streamlit!
-        """
 
 #with st.echo(code_location='below'):
 pages = ["Home", "Player Exploration", "Predictive Model"]
 choice = st.sidebar.selectbox("Select a page:", pages)
 if choice == "Home":
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png")
+        years = ["2022", "2021", "2020", "2019"]
+        year = st.selectbox(" ", years)
 elif choice == "Player Exploration":
         st.markdown("Select type of player:")
         st.radio(" ", ["Batter", "Pitcher"], horizontal=True)
